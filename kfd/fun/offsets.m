410a23af93cf307a1187cb86af2d1a9a815e000b
みぞれ゚ <68125875+m1zole@users.noreply.github.com>
2023-08-29 02:45:25 +0900

    Add 6s 15.7.3 offset credit: @34306
    英語で書く力残ってなかったから許して  
    まず、ucredには15.2以降(?)あたりで書き込めなくなった元のは絶対にこける  
    off_unknowmはkrw.mのやつだけど、全く別のところでこけるから後で直せばいい  
    あと、offsetはDopamineやらTrollStoreやらいろんなとこから持ってきてるけど、arm64eのだから間違ってるかもしれん  
    Dopamineのlibjailbreakd/utils.m見て貰えばわかるんだけど15.2以降は結構その場でoffset見つけなきゃいけなかったりする  
    
    最後に!なんと!iconcacheが超絶悪いタイミングで起こると、brickさせるから注意が必要だぞ!cowabunga以外でも起こりうるぞ!STOP風評被害!

--- kfd/fun/offsets.m
+++ kfd/fun/offsets.m
@@ -234,6 +234,89 @@ void _offsets_init(void) {
         off_proc_set_ucred = 0xfffffff00759a324;
         off_unknown = 0xfffffff0070ff160;
         
+    } else if(SYSTEM_VERSION_EQUAL_TO(@"15.7.3")) {
+
+        printf("[i] offsets selected for iOS 15.7.3\n");
+        //iPhone 6s 15.7.3 offsets
+
+        //https://github.com/apple-oss-distributions/xnu/blob/xnu-8019.41.5/bsd/sys/proc_internal.h#L227
+        off_p_list_le_prev = 0x8;
+        off_p_name = 0x381;
+        off_p_pid = 0x68;
+        off_p_ucred = 0;//0xd8
+        off_p_task = 0x10;
+        off_p_csflags = 0x1c;//0x300
+        off_p_uid = 0x2c;
+        off_p_gid = 0x30;
+        off_p_ruid = 0x34;
+        off_p_rgid = 0x38;
+        off_p_svuid = 0x44;//0x3c
+        off_p_svgid = 0x48;//0x40
+        off_p_textvp = 0x350;
+        off_p_pfd = 0xf8;//0x100
+
+        //https://github.com/apple-oss-distributions/xnu/blob/xnu-8019.41.5/bsd/sys/ucred.h#L91
+        off_u_cr_label = 0x78;
+        off_u_cr_uid = 0x18;
+        off_u_cr_ruid = 0x1c;
+        off_u_cr_svuid = 0x20;
+        off_u_cr_ngroups = 0x24;
+        off_u_cr_groups = 0x28;
+        off_u_cr_rgid = 0x68;
+        off_u_cr_svgid = 0x6c;
+
+        //https://github.com/apple-oss-distributions/xnu/blob/xnu-8019.41.5/osfmk/kern/task.h#L157
+        off_task_t_flags = 0x3e8;
+        off_task_itk_space = 0x308;//0x330
+        off_task_map = 0x28;    //_get_task_pmap
+
+        //https://github.com/apple-oss-distributions/xnu/blob/xnu-8019.41.5/osfmk/vm/vm_map.h#L471
+        off_vm_map_pmap = 0x40;
+
+        //https://github.com/apple-oss-distributions/xnu/blob/xnu-8019.41.5/osfmk/arm/pmap.h#L377
+        off_pmap_ttep = 0x8;
+
+        //https://github.com/apple-oss-distributions/xnu/blob/xnu-8019.41.5/bsd/sys/vnode_internal.h#L142
+        off_vnode_vu_ubcinfo = 0x78;
+        off_vnode_v_name = 0xb8;
+        off_vnode_v_parent = 0xc0;
+        off_vnode_v_data = 0xe0; // 0xd8?
+        
+        off_fp_glob = 0x10;
+
+        off_fg_data = 0x38;
+
+        //https://github.com/apple-oss-distributions/xnu/blob/xnu-8019.41.5/bsd/sys/ubc_internal.h#L149
+        off_ubc_info_cs_blobs = 0x50;
+
+        //https://github.com/apple-oss-distributions/xnu/blob/xnu-8019.41.5/bsd/sys/ubc_internal.h#L102
+        off_cs_blob_csb_platform_binary = 0xac;//0xb8
+
+        //https://github.com/apple-oss-distributions/xnu/blob/xnu-8019.41.5/osfmk/ipc/ipc_port.h#L152
+        //https://github.com/0x7ff/dimentio/blob/7ffffffb4ebfcdbc46ab5e8f1becc0599a05711d/libdimentio.c#L958
+        off_ipc_port_ip_kobject = 0x48; //0x58
+
+        //https://github.com/apple-oss-distributions/xnu/blob/xnu-8019.41.5/osfmk/ipc/ipc_space.h#L128
+        off_ipc_space_is_table = 0x20;
+
+        off_amfi_slot = 0x8;
+        off_sandbox_slot = 0x10;
+
+        off_kalloc_data_external = 0xFFFFFFF00719762C;	//Maybe restruct but it's still here
+        off_kfree_data_external = 0xFFFFFFF007197C98;	//One more function restruct
+        off_add_x0_x0_0x40_ret = 0xFFFFFFF005C79EBC;	//Yep, struct same but i need to + 0x4
+        off_empty_kdata_page = 0xFFFFFFF0077E0000 + 0x100;	//Kinna same struct
+        off_trustcache = 0xFFFFFFF007876530;		//struct unchange
+        off_gphysbase = 0xFFFFFFF007116740; //xref pmap_attribute_cache_sync size: 0x%llx @%s:%d, same struct
+        off_gphyssize = 0xFFFFFFF007116748; //xref pmap_attribute_cache_sync size: 0x%llx @%s:%d, same struct
+        off_pmap_enter_options_addr = 0xFFFFFFF00728C7F4;	//struct change but still remain
+        off_allproc = 0xFFFFFFF007856188;			//struct unchanged
+        off_pmap_find_phys = 0xFFFFFFF00729341C;		//struct unchanged
+        off_ml_phys_read_data = 0xFFFFFFF0072A3828;		//struct unchanged
+        off_ml_phys_write_data = 0xFFFFFFF0072A3A90;		//struct unchanged    
+        off_proc_set_ucred = 0xfffffff00759a324;
+        off_unknown = 0xfffffff0070ff160; // it wrong but i think resolt is the same
+        
     } else if(SYSTEM_VERSION_EQUAL_TO(@"15.6")) {
 
         printf("[i] offsets selected for iOS 15.6\n");
